5ec38b615b7371506d2511a3f01a05e3879b2959
Anton <anton@Polinas-MacBook-Air.local>
2021-12-01 15:16:57 -0500

    wip: added dependencies, middleware, listener

---
+++ server.js
@@ -0,0 +1,50 @@
+// Dependencies
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const paintingsController = require('./controllers/paintings');
+
+
+
+
+// Initialize app
+const app = express();
+
+// Configure settings
+require('dotenv').config();
+
+const { DATABASE_URL, PORT } = process.env;
+
+// Database Connection
+mongoose.connect(DATABASE_URL);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB')
+});
+
+// mount middleware
+app.use(express.static('public'));
+
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(methodOverride('_method'));
+
+// mount routes
+app.use('/paintings', paintingsController);
+
+
+
+app.get('/', (req, res) => {
+    res.redirect('/paintings')
+ })
+
+
+
+// Listener
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port: ${PORT}`)
+});
+
+
+// console.log(module)
