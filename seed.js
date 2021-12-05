412e2f3d4218c83baf0a3b1a7d97be9431453d22
Anton <anton@Polinas-MacBook-Air.local>
2021-12-05 13:38:32 -0500

    added connection to mongo

--- seed.js
+++ seed.js
@@ -1,7 +1,10 @@
+const mongoose = require("mongoose");
 const axios = require("axios").default;
 const clientID = "c25d934959503b1ea1a7";
 const clientSecret = "f891221a8c9cc22b4bd33be14d0e1a28";
 const apiUrl = "https://api.artsy.net/api/tokens/xapp_token";
+
+const Painting = require("./models/painting");
 
 const getToken = async () => {
   const res = await axios.post(apiUrl, {
@@ -27,12 +30,53 @@ const getArtworks = async (limit) => {
     const res = await api.get(next);
     artworks.push(...res.data._embedded.artworks);
     next = res.data._links.next.href;
-    console.log(artworks.length);
+    await new Promise((resolve) => setTimeout(resolve, 500));
   }
 
   return artworks;
 };
 
-getArtworks(100).then((artworks) =>
-  console.log("got artworks", artworks.length)
-);
+const transformArtwork = (artwork) => {
+  const name = artwork.slug;
+  const title = artwork.title;
+  const date = artwork.date;
+  const dimensions = artwork.dimensions;
+  const larger = artwork.image_versions.larger;
+  const image = artwork._links(image);
+  const medium = artwork.medium;
+  //console.log(artwork);
+  // TODO get the necessary data from the artwork object
+  // TODO return artwork data in correct schema
+  return {
+    name,
+    title,
+    date,
+    dimensions,
+    image,
+    medium,
+  };
+};
+
+// Configure settings
+require("dotenv").config();
+
+const { DATABASE_URL, PORT } = process.env;
+
+// Database Connection
+mongoose.connect(DATABASE_URL);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+
+const loadArtworksIntoDb = (artworks) => {
+  // TODO connect to mongodb and write data
+  Painting.collection.insertMany(artworks, () => mongoose.connection.close());
+};
+
+getArtworks(10).then((artworks) => {
+  // transform artworks to correct schema
+  const transformedArtworks = artworks.map(transformArtwork);
+
+  // load artworks into mongodb
+  loadArtworksIntoDb(transformedArtworks);
+});
